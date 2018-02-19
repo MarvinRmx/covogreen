8d41ac6da6670edcf83d50376fca6c05513b24c7
em612865 <karmoudimohamed@gmail.com>
2018-02-19 15:17:31 +0100

    Mise à jour formulaire recherche

--- covogreen-front/src/app/recherche-page/recherche-page.component.ts
+++ covogreen-front/src/app/recherche-page/recherche-page.component.ts
@@ -6,89 +6,98 @@ import {isNullOrUndefined} from 'util';
 import {TrajetEnt} from '../../class/TrajetEnt';
 
 @Component({
-  selector: 'app-recherche-page',
-  templateUrl: './recherche-page.component.html',
-  styleUrls: ['./recherche-page.component.css'],
+    selector: 'app-recherche-page',
+    templateUrl: './recherche-page.component.html',
+    styleUrls: ['./recherche-page.component.css'],
     providers: [RechercheTrajetService]
 })
 export class RecherchePageComponent implements OnInit {
-  rechercheFormEnt: RechercheFormEnt;
-  offres: TrajetEnt[];
+    rechercheFormEnt: RechercheFormEnt;
+    offres: TrajetEnt[];
 
-  error: boolean;
-  messagesErreur: string[];
-  submitted: boolean;
+    error: boolean;
+    messagesErreur: string[];
+    submitted: boolean;
 
-  nb_total_page: number[];
-  currentPage: number;
+    nb_total_page: number[];
+    currentPage: number;
 
-  constructor(private route: ActivatedRoute, private rechercheService: RechercheTrajetService, private router: Router) {
-      this.rechercheFormEnt = new RechercheFormEnt(null, null, false, null);
-      this.offres = [];
-      this.error = false;
-      this.submitted = false;
-      this.nb_total_page = [];
-  }
+    constructor(private route: ActivatedRoute, private rechercheService: RechercheTrajetService, private router: Router) {
+        this.rechercheFormEnt = new RechercheFormEnt(null, null, false, null);
+        this.offres = [];
+        this.error = false;
+        this.submitted = false;
+        this.nb_total_page = [];
+    }
 
-  get diagnostic() {
-      return JSON.stringify(this.offres);
-  }
+    get diagnostic() {
+        return JSON.stringify(this.offres);
+    }
 
-  loadTrajets(){
-      // On récupère les informations passé en paramètre/
-      this.route.queryParams.subscribe(params => {
-
-              //if ( !isNullOrUndefined(params.depart) && !isNullOrUndefined(params.destination) && !isNullOrUndefined(params.date_trajet) && !isNullOrUndefined(params.place_libre) && !isNullOrUndefined(params.page) ) {
-        this.rechercheFormEnt.depart = params.depart;
-        this.rechercheFormEnt.destination = params.destination;
-        this.rechercheFormEnt.date_trajet = params.date_trajet;
-        this.rechercheFormEnt.place_libre = params.place_libre;
-        this.currentPage = params.page;
+    loadTrajets(rechercheEnt: RechercheFormEnt, page: number){
+        this.rechercheFormEnt.depart      = rechercheEnt.depart;
+        this.rechercheFormEnt.destination = rechercheEnt.destination;
+        this.rechercheFormEnt.date_trajet = rechercheEnt.date_trajet;
+        this.rechercheFormEnt.place_libre = rechercheEnt.place_libre;
+        this.currentPage                  = page;
 
         // On récupère les trajets depuis le backend.
-        this.rechercheService.getTrajets(this.rechercheFormEnt, this.currentPage).subscribe(
-        (res: Response) => { // on récupère la réponse.
+        this.rechercheService.getTrajets(this.rechercheFormEnt, this.currentPage).subscribe((res: Response) => { // on récupère la réponse.
             this.submitted = true;
             // On vérifie la variable 'success'.
             // Offres existantent.
             if (res['success'] === true) {
-                // On stocke les trajet dans la variables offres.
-                this.offres = <TrajetEnt[]>res['trajets'];
-                this.error = false;
+                // On stocke les trajets dans la variables offres.
+                this.offres   = <TrajetEnt[]>res['trajets'];
+                this.error    = false;
                 this.createArrayPagination(res['nb_total_page']);
             } else if (res['success'] === false) {
                 // On affiche le message d'erreur
                 // Aucune offre ou autre
                 this.error = true;
                 this.messagesErreur = <string[]>res['message'];
-                }
-            });
-             // } else {
-               //   console.log('recherche-page-component.tx : Formulaire non envoyé');
-              //}
-          }
-      );
-  }
+            }
+        });
+    }
 
-  ngOnInit() {
-      this.loadTrajets();
-  }
+    ngOnInit() {
+        // On récupère les informations passé en paramètre
+        this.route.queryParams.subscribe(params => {
+            // L'utilisateur a complété les champs du formulaire.
+            if(params.depart && params.destination && params.date_trajet && params.place_libre && params.page){
+                this.loadTrajets(new RechercheFormEnt(
+                    params.depart,
+                    params.destination,
+                    params.date_trajet,
+                    params.place_libre
+                ), params.page);
+            }else{
+                // Première visite sur la page recherche.
+                this.loadTrajets(new RechercheFormEnt(
+                    "",
+                    "",
+                    "",
+                    ""
+                ), 1);
+            }
+        });
+    }
 
-  createArrayPagination(num: number) {
-      for (let i = 1; i <= num; i++) {
-        this.nb_total_page[i - 1] = i;
-      }
-  }
+    createArrayPagination(num: number) {
+        for (let i = 1; i <= num; i++) {
+            this.nb_total_page[i - 1] = i;
+        }
+    }
 
     paginationPage(page: number) {
-      this.router.navigate(['/recherche'], {
-          queryParams: {
-              'depart': this.rechercheFormEnt.depart,
-              'destination': this.rechercheFormEnt.destination,
-              'date_trajet': this.rechercheFormEnt.date_trajet,
-              'place_libre': this.rechercheFormEnt.place_libre,
-              'page': page
-          }
-      });
+        this.router.navigate(['/recherche'], {
+            queryParams: {
+                'depart': this.rechercheFormEnt.depart,
+                'destination': this.rechercheFormEnt.destination,
+                'date_trajet': this.rechercheFormEnt.date_trajet,
+                'place_libre': this.rechercheFormEnt.place_libre,
+                'page': page
+            }
+        });
     }
 }
