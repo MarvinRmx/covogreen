713b5d26a42d12b36505ff99fc115c7e6d6fcfe9
yanicetrech <34682577+yanicetrech@users.noreply.github.com>
2018-03-19 16:18:04 +0100

    Add files via upload

---
+++ WebAPI-Maps/index.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    path = require('path'),
+    jade = require('jade'),
+    coffee = require('./routes/coffee'),
+    pizza = require('./routes/pizza');
+    city = require('./routes/city');
+
+var app = new express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/coffee', coffee);
+app.use('/pizza', pizza);
+app.use('/city', city);
+
+app.get('/',function(req,res){
+  res.render('layout', { title: 'Node.js / Google Maps Example', subtitle: 'with the help of the Express, Path, and Jade modules' });
+});
+
+app.listen(3000)
