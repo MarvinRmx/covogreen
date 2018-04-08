cab9bb26c4f1e368aa445d486b78f4f4333e077d
Alex zarzitski <alex.zarzitski@gmail.com>
2018-04-08 19:47:40 +0200

    bug fix

--- database.sql
+++ database.sql
@@ -80,8 +80,8 @@ CREATE TABLE `inscriptionjourneys` (
   `updatedAt` datetime NOT NULL,
   `id_user` int(11) DEFAULT NULL,
   `id_trajet` int(11) DEFAULT NULL,
-  `rate` int(11) NOT NULL
-  `comment` varchar(500) NOT NULL
+  `rate` int(11) NOT NULL,
+  `comment` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
