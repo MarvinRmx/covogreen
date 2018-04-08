9379750186760162c16a2fdab6d76b58c16c255a
Alex zarzitski <alex.zarzitski@gmail.com>
2018-04-08 17:33:30 +0200

    test

--- covogreen-back/test/InscriptionTrajet.test.js
+++ covogreen-back/test/InscriptionTrajet.test.js
@@ -28,23 +28,11 @@ describe('InscriptionTrajet', function () {
       };
   });
 
-  // //verifi si l'utilisateur est inscrit retour erreur
-  // it('verifierInscriptionAloradySubscribed', function(done) {
-  //     request(app).post('/inscriptionTrajet/verif').set('Authorization', 'bearer ').set(headersUserRomain)
-  //     .send({ idTrajet: 5 }).expect(200).end(function(err, res) {
-  //         if (err)
-  //           return done(err);
-  //         else
-  //           console.log('Result:', res.text);
-  //         done();
-  //     });
-  // });
-
-  //inscrit l'utilisateur retour pas d'erreur
-  describe('verifierSubscribeUser()', function () {
+  //verifi si l'utilisateur est inscrit retour erreur
+  describe('verifierInscriptionIfAlreadySubscribed', function () {
     it('should accept', function testLogin (done) {
-      request(app).get('/inscriptionTrajet/byjourneyuser/7').set('Authorization', 'bearer ')
-      .set(headersUserRomain).send({ idTrajet: 7 }).expect(200).end(function(err, res) {
+      request(app).post('/inscriptionTrajet/verif').set('Authorization', 'bearer ').set(headersUserRomain)
+      .send({ idTrajet: 5 }).expect(200).end(function(err, res) {
           if (err)
             return done(err);
           else
@@ -53,4 +41,18 @@ describe('InscriptionTrajet', function () {
       });
     });
   });
+
+  //inscrit l'utilisateur retour pas d'erreur
+  // describe('verifierSubscribeUser()', function () {
+  //   it('should accept', function testLogin (done) {
+  //     request(app).get('/inscriptionTrajet/byjourneyuser/7').set('Authorization', 'bearer ')
+  //     .set(headersUserRomain).send({ idTrajet: 7 }).expect(200).end(function(err, res) {
+  //         if (err)
+  //           return done(err);
+  //         else
+  //           console.log('Result:', res.text);
+  //         done();
+  //     });
+  //   });
+  // });
 });
