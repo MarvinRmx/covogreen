5fd23d577bffd313fd2e874351d1269150c2e561
Alex zarzitski <alex.zarzitski@gmail.com>
2018-04-08 19:48:12 +0200

    ...

--- database.sql
+++ database.sql
@@ -81,7 +81,7 @@ CREATE TABLE `inscriptionjourneys` (
   `id_user` int(11) DEFAULT NULL,
   `id_trajet` int(11) DEFAULT NULL,
   `rate` int(11) NOT NULL,
-  `comment` varchar(250) NOT NULL
+  `comment` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
